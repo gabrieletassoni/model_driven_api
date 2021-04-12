67f2c3f5862b89b6cdf763591d10a04e4d9d2dc7
Gabriele Tassoni <gabrieletassoni@alchemic.it>
2021-04-12 19:48:04 +0000

    .presence||ENV["SECRET_KEY_BASE"]

--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,11 +1,11 @@
 class JsonWebToken
     class << self
       def encode(payload, expiry = 15.minutes.from_now.to_i)
-        ::JWT.encode(payload.merge(exp: expiry), ::Rails.application.credentials.dig(:secret_key_base))
+        ::JWT.encode(payload.merge(exp: expiry), ::Rails.application.credentials.dig(:secret_key_base).presence||ENV["SECRET_KEY_BASE"])
       end
       
       def decode(token)
-        body = ::JWT.decode(token, ::Rails.application.credentials.dig(:secret_key_base))[0]
+        body = ::JWT.decode(token, ::Rails.application.credentials.dig(:secret_key_base).presence||ENV["SECRET_KEY_BASE"])[0]
         ::HashWithIndifferentAccess.new body
       rescue
         nil
