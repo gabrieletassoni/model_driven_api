4aa99b62337ae4d86177f2d79cf11cc642961146
Gabriele Tassoni <g.tassoni@bancolini.com>
2023-03-07 12:23:45 +0000

    Fix Cors problem with token

--- config/initializers/cors_api_thecore.rb
+++ config/initializers/cors_api_thecore.rb
@@ -1,21 +1,14 @@
-# config/initializers/cors.rb
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins '*'
-#     resource '*',
-#       headers: %w(Token),
-#       methods: :any,
-#       expose: %w(Token),
-#       max_age: 600
-#   end
-# end
-
 puts "Loading CORS"
+# config/initializers/cors_api_thecore.rb
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  # Allow Everything
-  # Please override to your specific security needs in the actual application
+  #   # Allow Everything
+  #   # Please override to your specific security needs in the actual application
   allow do
     origins '*'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head]
+    resource '*',
+      headers: %w(app lang enc-data user-data session-id x-requested-with content-type origin authorization accept client-security-token Accept Authorization Cache-Control Content-Type DNT If-Modified-Since Keep-Alive Origin User-Agent X-Requested-With Token),
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      expose: %w(authorization Authorization Content-Length Token),
+      max_age: 600
   end
 end
